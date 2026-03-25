4efefbbd36f96f49b2fc229afa43e31e1ab5f16a
ShinJeongBeom <dingfan36@gmail.com>
2026-03-25 17:15:48 +0900

    feat: Member 엔티티 및 Repository 구현, 테스트 완료

--- src/main/java/com/jeongbeom/ecommerce/member/repository/MemberRepository.java
+++ src/main/java/com/jeongbeom/ecommerce/member/repository/MemberRepository.java
@@ -1,4 +1,10 @@
 package com.jeongbeom.ecommerce.member.repository;
 
-public interface MemberRepository {
+import com.jeongbeom.ecommerce.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
 }
